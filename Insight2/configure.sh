5fc60c004fe3d78379c7e839c43d4cbbf1d05408
Brad Gulko <bgulko@cs.cornell.edu>
2018-06-15 23:25:32 -0400

    Added code that automatically (Insight2/configure.sh) that checks dependencies, builds the software, downloads and decompresses databases during install. There is seperate code (Insight2/tests/runtests.sh) that prepares and runs all tests and prints diagnostic messages. --Brad

---
+++ Insight2/configure.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+sarg="$1"
+if [[ "$sarg" != "clean" ]]; then sarg="make"; fi
+
+echo -e "`date` Configuring Insight2 code\n\tmode: $sarg \n\t directory `pwd` \n\t (directory `readlink -f .` )\n"
+
+dbase=".."
+source "${dbase}/bin/lib.configure.sh"
+
+if [[ ! -x "${dbase}/bin/unstarch" ]]; then
+  echo -e "\n`date` Insight2 requires root configuration, configuring root."
+  pushd "../bin"
+  ./configure.sh
+  popd
+  if [[ ! -x "${dbase}/bin/unstarch" ]]; then
+    echo -e "\n`date` root configuration failed. Error. Exiting."
+    exit 1
+  fi
+fi
+
+# set up binary
+
+fbin="./bin/Insight2"
+
+if [[ "$sarg" == "clean" ]]; then
+  echo -e "Removing binary $fbin ."
+  rm -f "$fbin"
+else 
+  if [[ -x "$fbin" ]]; then
+    echo -e "\t Found binary $fbin ."
+  else
+    echo -e "\t Attempting to buind binary $fbin ."
+    make
+    if [[ ! -x "$fbin" ]]; then
+      echo -e "\t build failed - Attempting to expand precompiled binary $fbin."
+      expandFile "./bin/Insight2.bz2"
+    fi
+  fi
+  
+  if [[ -x "$fbin" ]]; then
+    echo -e "\t Found binary $fbin ."
+  else
+    echo -e "\t Unable to generate bianry. Error. Exiting."
+    exit 1
+  fi
+fi
+
+echo " "
+
+  
+
+# fetchign DB
+echo "`date` Configuring Insight2 database."
+
+fdb="Insight2DB.tar"
+furl='http://nextgen.cshl.edu/~bgulko/research/Insight2/db/FitCons2/Insight2DB.tar'
+
+if [[ "$sarg" == "clean" ]]; then
+  echo -e "`date`\tClearing  database in ./db ."
+  rm -vf ./db/*
+  echo " "
+  echo -e "`date`\tRemoving downloaded database archive $fdb ."
+  rm -vf "$fdb"
+  echo " "
+else
+  if [[ -s "$fdb" ]] ; then
+    echo -e "`date`\t Found database file $fdb . "
+  else
+    echo -e "`date`\t Retrieving database file $fdb from $furl ."
+    wget "${furl}"
+  fi
+
+  if [[ ! -s "$fdb" ]] ; then
+    echo -e "`date`\t Did not find database in $fdb - build failed. Exiting."
+    exit 1
+  fi
+  
+  echo -e "`date`\t Extracting database elements to ./db ."
+  tar -xvf "$fdb" -C "./db"
+  echo " "
+
+  echo -e "`date`\t Decompressing database elements in ./db ."
+  flist="block.bedg.starch monoDB.db.gz poly.bedg.starch polyn.bedg.starch"
+  for fin in ${flist}; do
+    echo -e "`date`\t\t Expanding $fin "
+    expandFile "./db/${fin}"
+    fout="./db/${fin%.*}" 
+    if [[ ! -s "$fout" ]]; then
+      echo -e "`date`\t\t unable to find $fout . Error. Exiting. "
+      exit 1
+    fi
+  done
+
+  echo " "
+  ls -l ./db/*
+  echo -e "\n`date`\t Database present. "
+fi
+
+echo " "
+
+echo -e "\n`date`\t Action Completed sucessfully. Exiting."
+
---
+++ Insight2/tests/runtests.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+sarg="$1"
+dbase="../.."
+source "${dbase}/bin/lib.configure.sh"
+# Insight2db from lib
+#insight2 executable from lib...
+fbin="${fiex}"
+
+echo -e "`date` Running Insight2 demos. \n\t directory `pwd` \n\t (directory `readlink -f .` )\n"
+
+if [[ ! -x "$fbin" ]]; then
+  echo -e "`date`\tUnable to find Insight2 executable ${fbin} . Please configure Insight2 first. Failure. Exiting.\n"
+  exit 1
+fi
+
+echo -e "\n`date` Starting Demo 0: Command line argument list"
+${fbin} -h
+echo -e "\n`date` Demo 0 complete.\n\n"
+
+
+echo -e "\n\n\n`date` Starting Demo 1: HG19"
+
+dt="1-hg19"
+pushd "$dt"
+
+fin="hg19.bed"
+echo -e "\t${fbin}  ${fidb} -fin $fin -qmap "
+${fbin}  "${didb}" -fin "$fin" -qmap 
+
+popd
+echo -e "\n`date` Demo 1 complete.\n\n"
+
+
+
+echo -e "\n\n\n`date` Starting Demo 2: Weighted positions (FitCons2 class ID 17,  genome-wide priors)."
+
+dt="2-weighted"
+pushd "$dt"
+
+# expand input file if needed 
+fin="FC2-017.bed"
+echo -e "\t`date` Verifying input file $fin ."
+expandFile "${fin}.starch"
+
+echo -e "\t`date`\n\t${fbin}  ${fidb} -fin $fin -qmap -maxsampw 115"
+${fbin}  "${didb}" -fin "$fin" -qmap -maxsampw 115
+
+popd
+echo -e "\n`date` Demo 2 complete.\n\n"
+
+
+
+echo -e "\n\n\n`date` Starting Demo 3: Posterior and expectation - (FitCons2 class ID 17, parental priors)"
+echo -e "\tNOTE: this uses 8 cores on the host computer. "
+
+dt="3-posterior"
+pushd "$dt"
+
+# expand input file if needed 
+fin="FC2-017.bed"
+echo -e "\t`date` Verifying input file $fin ."
+expandFile "${fin}.starch"
+
+echo -e "\t`date`\n\t${fbin}  ${fidb} -fin $fin -qmap -maxsampw 115  -rho 0.359788,1 -eta 0.788613,1 -gamma 1.277413,1 -qexp -nthread 8 -gres 10 -expval -expdist"
+${fbin}  "${didb}" -fin "$fin" -qmap -maxsampw 115 -rho 0.359788,1 -eta 0.788613,1 -gamma 1.277413,1 -qexp -nthread 16 -gres 8 -expval -expdist
+
+popd
+echo -e "\n`date` Demo 3 complete.\n\n"
+
+
+echo -e "\n\nTo compare demo resuts to expected output, compare results in each directory (e.g. 1-hg19) with fiels in ref subdirectories (e.g. 1-ht19/ref).\n"
+
+echo -e "\n`date` All Demos compelte. Exiting."
+
